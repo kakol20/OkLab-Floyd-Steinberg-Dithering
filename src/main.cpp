57896b69af17f91e8c49ff9db815a7c62e8e2145
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2024-04-10 23:44:19 +0100

    Add NRoot function

--- src/main.cpp
+++ src/main.cpp
@@ -41,6 +41,8 @@ int main(int argc, char* argv[]) {
     if (inputImage.Read(imageFile.c_str(), 3)) {
       Image copyImage(inputImage);
 
+      const bool dithered = palettesJSON["dither"];
+
       for (int y = 0; y < copyImage.GetHeight(); y++) {
         for (int x = 0; x < copyImage.GetWidth(); x++) {
           const size_t index = inputImage.GetIndex(x, y);
@@ -54,58 +56,59 @@ int main(int argc, char* argv[]) {
           copyImage.SetData(index + 1, newPixel.GetGUInt());
           copyImage.SetData(index + 2, newPixel.GetBUInt());
 
-          const sRGB quant_error = oldpixel - newPixel;
+          if (dithered) {
+            const sRGB quant_error = oldpixel - newPixel;
 
-          if (y + 1 < copyImage.GetHeight()) {
-            const size_t qe_5 = copyImage.GetIndex(x, y + 1);
-            sRGB new_5(double(copyImage.GetData(qe_5 + 0)) / 255.,
-              double(copyImage.GetData(qe_5 + 1)) / 255.,
-              double(copyImage.GetData(qe_5 + 2)) / 255.);
-            new_5 = new_5 + (quant_error * (5. / 16.));
-            new_5.Clamp();
-            copyImage.SetData(qe_5 + 0, new_5.GetRUInt());
-            copyImage.SetData(qe_5 + 1, new_5.GetGUInt());
-            copyImage.SetData(qe_5 + 2, new_5.GetBUInt());
+            if (y + 1 < copyImage.GetHeight()) {
+              const size_t qe_5 = copyImage.GetIndex(x, y + 1);
+              sRGB new_5(double(copyImage.GetData(qe_5 + 0)) / 255.,
+                double(copyImage.GetData(qe_5 + 1)) / 255.,
+                double(copyImage.GetData(qe_5 + 2)) / 255.);
+              new_5 = new_5 + (quant_error * (5. / 16.));
+              new_5.Clamp();
+              copyImage.SetData(qe_5 + 0, new_5.GetRUInt());
+              copyImage.SetData(qe_5 + 1, new_5.GetGUInt());
+              copyImage.SetData(qe_5 + 2, new_5.GetBUInt());
+
+              if (x + 1 < copyImage.GetWidth()) {
+                const size_t qe_1 = copyImage.GetIndex(x + 1, y + 1);
+                sRGB new_1(double(copyImage.GetData(qe_1 + 0)) / 255.,
+                  double(copyImage.GetData(qe_1 + 1)) / 255.,
+                  double(copyImage.GetData(qe_1 + 2)) / 255.);
+                new_1 = new_1 + (quant_error * (1. / 16.));
+                new_1.Clamp();
+                copyImage.SetData(qe_1 + 0, new_1.GetRUInt());
+                copyImage.SetData(qe_1 + 1, new_1.GetGUInt());
+                copyImage.SetData(qe_1 + 2, new_1.GetBUInt());
+              }
+
+              if (x - 1 >= 0) {
+                const size_t qe_3 = copyImage.GetIndex(x - 1, y + 1);
+                sRGB new_3(double(copyImage.GetData(qe_3 + 0)) / 255.,
+                  double(copyImage.GetData(qe_3 + 1)) / 255.,
+                  double(copyImage.GetData(qe_3 + 2)) / 255.);
+                new_3 = new_3 + (quant_error * (3. / 16.));
+                new_3.Clamp();
+                copyImage.SetData(qe_3 + 0, new_3.GetRUInt());
+                copyImage.SetData(qe_3 + 1, new_3.GetGUInt());
+                copyImage.SetData(qe_3 + 2, new_3.GetBUInt());
+              }
+            }
 
             if (x + 1 < copyImage.GetWidth()) {
-              const size_t qe_1 = copyImage.GetIndex(x + 1, y + 1);
-              sRGB new_1(double(copyImage.GetData(qe_1 + 0)) / 255.,
-                double(copyImage.GetData(qe_1 + 1)) / 255.,
-                double(copyImage.GetData(qe_1 + 2)) / 255.);
-              new_1 = new_1 + (quant_error * (1. / 16.));
-              new_1.Clamp();
-              copyImage.SetData(qe_1 + 0, new_1.GetRUInt());
-              copyImage.SetData(qe_1 + 1, new_1.GetGUInt());
-              copyImage.SetData(qe_1 + 2, new_1.GetBUInt());
+              const size_t qe_7 = copyImage.GetIndex(x + 1, y);
+              sRGB new_7(double(copyImage.GetData(qe_7 + 0)) / 255.,
+                double(copyImage.GetData(qe_7 + 1)) / 255.,
+                double(copyImage.GetData(qe_7 + 2)) / 255.);
+              new_7 = new_7 + (quant_error * (7. / 16.));
+              new_7.Clamp();
+              copyImage.SetData(qe_7 + 0, new_7.GetRUInt());
+              copyImage.SetData(qe_7 + 1, new_7.GetGUInt());
+              copyImage.SetData(qe_7 + 2, new_7.GetBUInt());
             }
-
-            if (x - 1 >= 0) {
-              const size_t qe_3 = copyImage.GetIndex(x - 1, y + 1);
-              sRGB new_3(double(copyImage.GetData(qe_3 + 0)) / 255.,
-                double(copyImage.GetData(qe_3 + 1)) / 255.,
-                double(copyImage.GetData(qe_3 + 2)) / 255.);
-              new_3 = new_3 + (quant_error * (3. / 16.));
-              new_3.Clamp();
-              copyImage.SetData(qe_3 + 0, new_3.GetRUInt());
-              copyImage.SetData(qe_3 + 1, new_3.GetGUInt());
-              copyImage.SetData(qe_3 + 2, new_3.GetBUInt());
-            }
-          }
-
-          if (x + 1 < copyImage.GetWidth()) {
-            const size_t qe_7 = copyImage.GetIndex(x + 1, y);
-            sRGB new_7(double(copyImage.GetData(qe_7 + 0)) / 255.,
-              double(copyImage.GetData(qe_7 + 1)) / 255.,
-              double(copyImage.GetData(qe_7 + 2)) / 255.);
-            new_7 = new_7 + (quant_error * (7. / 16.));
-            new_7.Clamp();
-            copyImage.SetData(qe_7 + 0, new_7.GetRUInt());
-            copyImage.SetData(qe_7 + 1, new_7.GetGUInt());
-            copyImage.SetData(qe_7 + 2, new_7.GetBUInt());
           }
         }
       }
-
       const std::string outputImageFile = palettesJSON["output_image"];
       copyImage.Write(outputImageFile.c_str());
     }
