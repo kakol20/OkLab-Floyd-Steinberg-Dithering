0721f1012e44b8649a8e25a5a76e4e9602387041
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2024-04-10 18:23:33 +0100

    Read json file

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,36 @@
 int main(int argc, char* argv[]) {
     //std::cout << "Hello World\n";
 
-    Log::StartLine();
+    /*Log::StartLine();
     Log::Write("Hello World!");
     Log::EndLine();
+
+    Log::Save();*/
+
+    std::ifstream f("palettes.json");
+
+
+    if (f) {
+        std::map<std::string, std::vector<std::string>> palettes;
+        json palettesJSON = json::parse(f);
+
+        const auto& palettesStr = palettesJSON["palettes"];
+        Log::StartLine();
+        Log::Write("Chosen Palette: ");
+        Log::Write(palettesJSON["chosen_palette"]);
+        Log::EndLine();
+
+        for (auto& [key, val] : palettesStr.items()) {
+            Log::StartLine();
+            Log::Write(key + '\n');
+            for (auto& colors : palettesStr[key]) {
+                const auto& hex = colors.get<std::string>();
+                palettes[key].push_back(hex);
+
+                Log::Write("  #" + hex + '\n');
+            }
+        }
+    }
 
     Log::Save();
 
