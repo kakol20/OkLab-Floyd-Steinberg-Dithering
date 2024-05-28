b41cc71da011eced37b77df2b8e5d0b95e82e9a7
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2024-05-28 16:02:13 +0100

    Read palette file

--- src/main.cpp
+++ src/main.cpp
@@ -9,23 +9,123 @@ const double Maths::RadToDeg = 180. / Maths::Pi;
 const double Maths::DegToRad = Maths::Pi / 180.;
 
 int main(int argc, char* argv[]) {
-  /*std::ifstream f("settings.json");
+  //std::cout << "\nPress enter to exit...\n";
+  //std::cin.ignore();
 
+  //GeneratePalette("data/minecraft_map_gs");
+
+  std::ifstream f("settings.json");
   if (f) {
-    
+    json settings = json::parse(f);
+
+    // ----- CHECK IF SETTING EXISTS -----
+
+    bool success = true;
+
+    if (!settings.contains("input_image")) {
+      Log::StartLine();
+      Log::Write("input setting not found");
+      Log::EndLine();
+      success = false;
+    }
+
+    if (!settings.contains("output_image")) {
+      Log::StartLine();
+      Log::Write("output_image setting not found");
+      Log::EndLine();
+      success = false;
+    }
+
+    if (!settings.contains("palette")) {
+      Log::StartLine();
+      Log::Write("palette setting not found");
+      Log::EndLine();
+      success = false;
+    }
+
+    if (!settings.contains("dither")) {
+      Log::StartLine();
+      Log::Write("dither setting not found");
+      Log::EndLine();
+      success = false;
+    }
+
+    if (!settings.contains("grayscale")) {
+      Log::StartLine();
+      Log::Write("grayscale setting not found");
+      Log::EndLine();
+      success = false;
+    }
+
+    if (!settings.contains("dist_lightness")) {
+      Log::StartLine();
+      Log::Write("dist_lightness setting not found");
+      Log::EndLine();
+      success = false;
+    }
+
+    if (success) {
+      // ----- READ SETTINGS -----
+
+      Log::StartLine();
+      Log::Write("----- SETTINGS -----");
+      Log::EndLine();
+
+      const std::string inputLoc = settings["input_image"];
+      Log::StartLine();
+      Log::Write("Input: " + inputLoc);
+      Log::EndLine();
+
+      const std::string outputLoc = settings["output_image"];
+      Log::StartLine();
+      Log::Write("Output: " + outputLoc);
+      Log::EndLine();
+
+      const std::string paletteLoc = settings["palette"];
+      Log::StartLine();
+      Log::Write("Palette: " + paletteLoc);
+      Log::EndLine();
+
+      const bool dither = settings["dither"];
+      Log::StartLine();
+      Log::Write("Dither: ");
+      Log::Write(dither ? "TRUE" : "FALSE");
+      Log::EndLine();
+
+      const bool grayscale = settings["grayscale"];
+      Log::StartLine();
+      Log::Write("Process As Grayscale: ");
+      Log::Write(grayscale ? "TRUE" : "FALSE");
+      Log::EndLine();
+
+      const bool dist_l = settings["dist_lightness"];
+      Log::StartLine();
+      Log::Write("Use Lightness As Distance: ");
+      Log::Write(dist_l ? "TRUE" : "FALSE");
+      Log::EndLine();
+
+      // ----- GET PALETTE -----
+
+      std::vector<sRGB> palette;
+      success = success && GetPalette(paletteLoc, palette);
+
+      // ----- GET INPUT IMAGE -----
+      Log::StartLine();
+      Log::Write("----- INPUT IMAGE -----");
+      Log::EndLine();
+
+      Image inputImg;
+      success = success && inputImg.Read(inputLoc.c_str());
+
+      if (success) {
+      }
+    }
   }
   else {
     Log::StartLine();
     Log::Write("Failed to read: settings.json");
     Log::EndLine();
   }
-
-  Log::Save();*/
-
-  //std::cout << "\nPress enter to exit...\n";
-  //std::cin.ignore();
-
-  GeneratePalette("data/minecraft_wool");
 
   Log::Save();
   return 0;
@@ -47,6 +147,47 @@ sRGB ClosestPaletteColorRGB(const std::vector<sRGB>& palette, const sRGB& col) {
 
   //return OkLab::OkLabtosRGB(closest);
   return sRGB();
+}
+
+bool GetPalette(const std::string& loc, std::vector<sRGB>& out) {
+  std::fstream p(loc);
+  if (p) {
+    Log::StartLine();
+    Log::Write("----- COLOURS -----");
+    Log::EndLine();
+
+    std::string hex;
+    while (std::getline(p, hex)) {
+      const sRGB rgb = sRGB::HexTosRGB(hex);
+      const OkLab lab = OkLab::sRGBtoOkLab(rgb);
+      const OkLCh lch = OkLCh::OkLabtoOkLCh(lab);
+
+      out.push_back(rgb);
+
+      Log::StartLine();
+      Log::Write("#");
+      Log::Write(hex);
+      Log::Write(" --- rgb(");
+      Log::Write(rgb.UintDebug());
+      Log::Write(") --- oklab(");
+      Log::Write(lab.Debug());
+      Log::Write(") --- oklch(");
+      Log::Write(lch.Debug());
+      Log::Write(")");
+      Log::EndLine();
+    }
+
+    return true;
+  }
+  else {
+    Log::StartLine();
+    Log::Write(loc);
+    Log::Write(" not found");
+    Log::EndLine();
+
+    p.close();
+    return false;
+  }
 }
 
 bool GeneratePalette(const std::string& baseFile) {
